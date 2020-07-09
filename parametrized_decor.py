a81be9ed011d3e9ee3bd434e1e10d22583ed1e7d
Sergey Illyushko <serhiiilliushko@gmail.com>
2020-07-09 10:59:52 +0300

    paramertized decor fixed v2

--- parametrized_decor.py
+++ parametrized_decor.py
@@ -1,12 +1,17 @@
 import os
+from datetime import datetime
 
 
-def parametrized_decor(parameter=os.path.abspath('result.py')):
+def parametrized_decor(parameter=os.path.abspath('result.txt')):
     def decor(foo):
         def new_foo(*args, **kwargs):
             result = foo(*args, **kwargs)
+            all_logs = f"Time when function was called, {datetime.now()},\n" \
+                       f"Function name, {parametrized_decor().__name__},\n" \
+                       f"All arguments: {args}, {kwargs},\n" \
+                       f"Returned values: {result}"
             with open(parameter, 'w', encoding='utf8') as file:
-                file.write(parameter)
+                file.write(all_logs)
             return result
         return new_foo
 
